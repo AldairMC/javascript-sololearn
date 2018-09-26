2aefb24d5e04283aeffb526800c4cb6e0fbaadc8
AldairMC <aldairmc2023@gmail.com>
2018-09-25 21:54:48 -0500

    emirp-numbers v2.0: finished

--- JavaScript/Emirp_Numbers.js
+++ JavaScript/Emirp_Numbers.js
@@ -1,11 +1,25 @@
 function eNumbers(n){
-    aux = n;
+    let aux = n;
     n += "";
-    n.split("").reverse().join("");
-    if(n % 2 == 0 && aux % 2 == 0){
+    var y = n.split("").reverse().join("");
+
+    if(eprime(y) && eprime(aux)){
+        return true +": "+ y +  " and " + aux + " is a prime number";
+    }
+    return false;
+}
+
+function eprime(x){
+    let acum = 0;
+    for(var i= 0; i <= x; i++){
+        if(x % i == 0){
+            acum += 1; 
+        }
+    }
+    if(acum === 2){
         return true;
     }
     return false;
 }
 
-console.log(eNumbers(64));
+console.log(eNumbers(17));
