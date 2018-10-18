3173b16446ed702d40eacb354ab50f8786a40876
AldairMC <aldairmc2023@gmail.com>
2018-10-17 22:16:09 -0500

    scientific-notation: v2.0

--- JavaScript/Scientific_Notation.js
+++ JavaScript/Scientific_Notation.js
@@ -2,16 +2,24 @@ function scientificNotatio(x){
 
     var aux=0;
     var cont=0;
+    var aux=[]; 
     x += "";
+    var w = x.split("");
     var y= x.split("").map(Number);
     console.log(y);
     for (let i = 0; i <= y.length; i++) {
         if(y[i] === 0){
             cont++;
         }
-        
     }
-    console.log();
+    for (let i = 0; i<w.length; i++){
+        if(w[i] !== "0"){
+            aux.push(w[i]);
+        }    
+    }
+    aux.join("").map(Number)
+    console.log(aux);
+    console.log(cont);
 }
 
-scientificNotatio(90000000000000);
+scientificNotatio(98700000000000);
