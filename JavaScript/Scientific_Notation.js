f43786dc9b6d7035f93364c7f7f2b4c8dc6409e7
AldairMC <aldairmc2023@gmail.com>
2018-10-19 23:05:56 -0500

    scientific-notation: v3.1

--- JavaScript/Scientific_Notation.js
+++ JavaScript/Scientific_Notation.js
@@ -1,26 +1,28 @@
 function scientificNotatio(x){
 
-    var aux=0;
     var cont=0;
     var aux=[]; 
     x += "";
     var w = x.split("");
     var y= x.split("").map(Number);
     console.log(y);
-    for (let i = 0; i <= y.length; i++) {
-        if(y[i] === 0){
-            cont++;
+    if(x > 1){
+        for (let i = 0; i <= y.length; i++) {
+            if(y[i] === 0){
+                cont++;
+            }
         }
+        for (let i = 0; i<w.length; i++){
+            if(w[i] !== "0"){
+                aux.push(w[i]);
+            }    
+        }
+        var z = aux.join("");
+        console.log(z);
+        console.log(cont);
+        console.log(z + "*10^" + cont);    
     }
-    for (let i = 0; i<w.length; i++){
-        if(w[i] !== "0"){
-            aux.push(w[i]);
-        }    
-    }
-    var z = aux.join("");
-    console.log(z);
-    console.log(cont);
-    console.log(z + "*10^" + cont);
+    
 }
 
 scientificNotatio(780000000000000);
