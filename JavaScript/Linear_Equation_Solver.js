d5cd5298caf820e1c170b87cfa7d1e1b89a7227b
AldairMC <aldairmc2023@gmail.com>
2018-10-27 20:19:30 -0500

    linear-equation-solver: v2.1 deleted spaces

--- JavaScript/Linear_Equation_Solver.js
+++ JavaScript/Linear_Equation_Solver.js
@@ -1,6 +1,14 @@
 function linear(str){
-    var aux = str.join("");
+    var aux = [];
+    for (let i = 0; i < str.length; i++) {
+        if(!(str[i] === " ")){
+            aux.push(str[i]);
+        }
+        
+    }
     console.log(aux);
 }
 
 linear('3x + 2  =1');
+
+//hacer un nuevo array y con push ir metiendo elementos 
