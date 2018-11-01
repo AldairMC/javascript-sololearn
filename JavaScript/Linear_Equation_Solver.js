b69eabb8bbe3587ac125bd6c6f129bac093ff12b
AldairMC <aldairmc2023@gmail.com>
2018-10-31 22:39:32 -0500

    linear-equation-solver: v2.5

--- JavaScript/Linear_Equation_Solver.js
+++ JavaScript/Linear_Equation_Solver.js
@@ -1,14 +1,18 @@
 function linear(str){
     var aux = [];
+    var op=[];
+    
     for (let i = 0; i < str.length; i++) {
         if(!(str[i] === " ")){
             aux.push(str[i]);
         }
-        
+        while(!(str[i]=== "=")){
+            op.push(str[i]);
+        }
     }
+    console.log(op);
     console.log(aux);
 }
 
 linear('3x + 2  =1');
 
-//hacer un nuevo array y con push ir metiendo elementos 
